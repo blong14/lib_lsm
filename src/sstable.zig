9201daac639e2b0492966ccbe203b1eb895276d1
Ben Long <blong14@lemur.localdomain>
2024-02-08 13:41:38 -0500

    Adding debug prints and protect against int overflow for sstable reading/writing

--- src/sstable.zig
+++ src/sstable.zig
@@ -37,9 +37,10 @@ pub const SSTable = struct {
         if (high < low) {
             return high + 1;
         }
-        const mid = (low + high) / 2;
+        const mid = low + ((high - low) / 2);
         const entry = try self.data.read(mid);
         if (key < entry.key) {
+            if (mid == 0) return mid;
             return self.findIndex(key, low, mid - 1);
         } else if (key == entry.key) {
             return mid;
@@ -58,7 +59,7 @@ pub const SSTable = struct {
 
     fn greaterthan(self: *Self, key: u64, idx: usize) bool {
         const entry = self.data.read(idx) catch |err| {
-            std.debug.print("Oops {s}\n", .{@errorName(err)});
+            std.debug.print("Oops greaterthan {s}\n", .{@errorName(err)});
             return false;
         };
         return key > entry.key;
@@ -74,7 +75,8 @@ pub const SSTable = struct {
     }
 
     pub fn write(self: *Self, key: u64, value: []const u8) Error!void {
-        const count: usize = self.data.count;
+        const count: usize = self.data.getCount();
+        std.debug.print("writing to sstable count {d}...\n", .{count});
         if ((count == 0) or (self.greaterthan(key, count - 1))) {
             return try self.data.append(SSTableRow{ .key = key, .value = value.ptr });
         }
@@ -100,11 +102,20 @@ test "SSTable" {
     defer st.deinit();
 
     // when
-    const key: u64 = std.hash.Murmur2_64.hash("__key__");
-    const expected: []const u8 = "__value__";
+    var key: u64 = std.hash.Murmur2_64.hash("__key__");
+    var expected: []const u8 = "__value__";
     try st.write(key, expected);
 
     // then
-    const actual = try st.read(key);
+    var actual = try st.read(key);
+    try testing.expect(std.mem.eql(u8, expected, actual));
+
+    // when
+    key = std.hash.Murmur2_64.hash("__key_a__");
+    expected = "__value_a__";
+    try st.write(key, expected);
+
+    // then
+    actual = try st.read(key);
     try testing.expect(std.mem.eql(u8, expected, actual));
 }
