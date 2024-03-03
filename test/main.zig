90f2c77e84043dcb6c62187e0e4d5bb489fe9fb0
Ben Long <blong14@lemur.localdomain>
2024-03-03 12:01:02 -0500

    Code cleanup for our WIP integration test

--- test/main.zig
+++ test/main.zig
@@ -1,72 +1,21 @@
 const std = @import("std");
 const lsm = @import("lsm");
 
+const msg = struct {
+    key: []const u8,
+    value: []const u8,
+};
+
 pub fn main() !void {
     var mainTimer = try std.time.Timer.start();
     const mainStart = mainTimer.read();
 
-    const msg = struct {
-        key: []const u8,
-        value: []const u8,
-    };
+    var garena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer garena.deinit();
 
-    var mailbox = try lsm.MessageQueue(msg).init(".");
+    const galloc = garena.allocator();
+    var mailbox = try lsm.MessageQueue(msg).init(galloc, ".");
     defer mailbox.deinit();
-
-    const writer = try std.Thread.spawn(.{}, struct {
-        pub fn publish(outbox: lsm.MessageQueue(msg).Writer) void {
-            defer outbox.done() catch |err| {
-                std.debug.print("publish error {s}\n", .{@errorName(err)});
-            };
-
-            var timer = std.time.Timer.start() catch |err| {
-                std.debug.print("publish error {s}\n", .{@errorName(err)});
-                return;
-            };
-            const start = timer.read();
-
-            const file = std.fs.cwd().openFile("data/input.csv", .{}) catch |err| {
-                std.debug.print("publish error {s}\n", .{@errorName(err)});
-                return;
-            };
-            defer file.close();
-
-            // TODO: Figure out an appropriate buffer size
-            var buffer = [_]u8{0} ** (4096 * 2000);
-            const reader = file.reader();
-            var csv_file = lsm.CSV.init(reader, &buffer, .{}) catch |err| {
-                std.debug.print("publish error {s}\n", .{@errorName(err)});
-                return;
-            };
-
-            const key: usize = 0;
-            const value: usize = 1;
-            var count: usize = 0;
-            var idx: usize = 0;
-            var row = [_][]const u8{undefined} ** 2;
-            while (csv_file.next() catch null) |token| {
-                switch (token) {
-                    .field => |val| {
-                        row[idx] = val;
-                        idx += 1;
-                    },
-                    .row_end => {
-                        const m: msg = .{ .key = row[key], .value = row[value] };
-                        outbox.publish(m) catch |err| {
-                            std.debug.print("error publishing row {s}\n", .{@errorName(err)});
-                            break;
-                        };
-                        row = [_][]const u8{undefined} ** 2;
-                        idx = 0;
-                        count += 1;
-                    },
-                }
-            }
-
-            const end = timer.read();
-            std.debug.print("total rows writen {d} in {}ms\n", .{ count, (end - start) / 1_000_000 });
-        }
-    }.publish, .{mailbox.publisher()});
 
     const reader = try std.Thread.spawn(.{}, struct {
         pub fn consume(inbox: lsm.MessageQueue(msg).ReadIter) void {
@@ -74,19 +23,18 @@ pub fn main() !void {
                 std.debug.print("consume error {s}\n", .{@errorName(err)});
                 return;
             };
-            const start = timer.read();
 
-            var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-            defer _ = gpa.deinit();
+            var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+            defer arena.deinit();
 
-            const alloc = gpa.allocator();
+            const alloc = arena.allocator();
             var db = lsm.defaultDatabase(alloc) catch |err| {
                 std.debug.print("consume error {s}\n", .{@errorName(err)});
                 return;
             };
             defer db.deinit();
-            errdefer db.deinit();
 
+            const start = timer.read();
             var count: usize = 0;
             while (inbox.next()) |row| {
                 db.write(row.key, row.value) catch |err| {
@@ -95,13 +43,54 @@ pub fn main() !void {
                 };
                 count += 1;
             }
-
             const end = timer.read();
             std.debug.print("total rows read {d} in {}ms\n", .{ count, (end - start) / 1_000_000 });
         }
     }.consume, .{mailbox.subscribe()});
+    const writer = mailbox.publisher();
 
-    writer.join();
+    const file = std.fs.cwd().openFile("data/input.csv", .{}) catch |err| {
+        std.debug.print("publish error {s}\n", .{@errorName(err)});
+        return;
+    };
+    defer file.close();
+
+    // TODO: Figure out an appropriate buffer size
+    var buffer = [_]u8{0} ** (std.mem.page_size * 2000);
+    const fileReader = file.reader();
+    var csv_file = lsm.CSV.init(fileReader, &buffer, .{}) catch |err| {
+        std.debug.print("publish error {s}\n", .{@errorName(err)});
+        return;
+    };
+
+    const writeStart = mainTimer.read();
+    var count: usize = 0;
+    var idx: usize = 0;
+    var row = [2][]const u8{ undefined, undefined };
+    while (csv_file.next() catch null) |token| {
+        switch (token) {
+            .field => |val| {
+                row[idx] = val;
+                idx += 1;
+            },
+            .row_end => {
+                const r: msg = .{ .key = row[0], .value = row[1] };
+                writer.publish(r) catch |err| {
+                    std.debug.print("error publishing row {s}\n", .{@errorName(err)});
+                    return;
+                };
+                count += 1;
+                row = [2][]const u8{ undefined, undefined };
+                idx = 0;
+            },
+        }
+    }
+    const writeEnd = mainTimer.read();
+    std.debug.print("total rows writen {d} in {}ms\n", .{ count, (writeEnd - writeStart) / 1_000_000 });
+    writer.done() catch |err| {
+        std.debug.print("publish error {s}\n", .{@errorName(err)});
+    };
+
     reader.join();
 
     const mainEnd = mainTimer.read();
