556fef5b4db4480349208c9670489d6ee6cc4893
Ben Long <blong14@lemur.localdomain>
2025-03-16 13:28:43 -0400

    Update MergeIterator to adhere to Iterator interface; adds ScanIterator

--- src/main.zig
+++ src/main.zig
@@ -30,6 +30,9 @@ pub const KV = @import("kv.zig").KV;
 const file = @import("file.zig");
 pub const OpenFile = file.open;
 
+const iter = @import("iterator.zig");
+pub const Iterator = iter.Iterator;
+
 const msgq = @import("msgqueue.zig");
 pub const ProcessMessageQueue = msgq.ProcessMessageQueue;
 pub const ThreadMessageQueue = msgq.Queue;
@@ -100,14 +103,17 @@ pub const Database = struct {
         var data_dir = try std.fs.openDirAbsolute(self.opts.data_dir, .{ .iterate = true });
         defer data_dir.close();
 
-        var count: u64 = 0;
+        debug.print("opening database @ {s}\n", .{self.opts.data_dir});
 
-        var dir_iter = data_dir.iterate();
+        var dir_iter = try data_dir.walk(self.alloc);
+        defer dir_iter.deinit();
+
+        var count: u64 = 0;
         while (try dir_iter.next()) |f| {
             const nxt_file = try std.fmt.allocPrint(
                 self.alloc,
                 "{s}/{s}",
-                .{ self.opts.data_dir, f.name },
+                .{ self.opts.data_dir, f.basename },
             );
             errdefer self.alloc.free(nxt_file);
 
@@ -128,10 +134,6 @@ pub const Database = struct {
 
             count += 1;
         }
-    }
-
-    pub fn allocator(self: Self) Allocator {
-        return self.alloc;
     }
 
     pub fn read(self: Self, key: []const u8) !KV {
@@ -161,9 +163,8 @@ pub const Database = struct {
 
     const MergeIterator = struct {
         alloc: Allocator,
-        mtbls: std.ArrayList(*Memtable.Iterator),
+        mtbls: std.ArrayList(*Iterator(KV)),
         queue: std.PriorityQueue(KV, void, lessThan),
-        v: ?KV,
 
         fn lessThan(context: void, a: KV, b: KV) Order {
             _ = context;
@@ -171,21 +172,27 @@ pub const Database = struct {
         }
 
         pub fn init(dalloc: Allocator, db: *const Database) !MergeIterator {
-            var iters = std.ArrayList(*Memtable.Iterator).init(dalloc);
+            const hot_table = db.mtable.load(.seq_cst);
+
+            var warm_tables: std.ArrayList(*Memtable) = undefined;
+            defer warm_tables.deinit();
             {
                 mtx.lock();
                 defer mtx.unlock();
-
-                const mtables = try dalloc.alloc(*Memtable.Iterator, db.mtables.items.len);
-                for (db.mtables.items, 0..) |mtable, i| {
-                    mtables[i].* = try mtable.iterator();
-                    try iters.append(mtables[i]);
-                }
+                warm_tables = try db.mtables.clone();
             }
 
-            const iter = try dalloc.create(Memtable.Iterator);
-            iter.* = try db.mtable.load(.seq_cst).iterator();
-            try iters.append(iter);
+            var iters = std.ArrayList(*Iterator(KV)).init(dalloc);
+
+            const it = try dalloc.create(Iterator(KV));
+            it.* = try hot_table.iterator(dalloc);
+            try iters.append(it);
+
+            for (warm_tables.items) |mtable| {
+                const mit = try dalloc.create(Iterator(KV));
+                mit.* = try mtable.iterator(dalloc);
+                try iters.append(mit);
+            }
 
             var queue = std.PriorityQueue(KV, void, lessThan).init(dalloc, {});
             try queue.ensureTotalCapacity(std.mem.page_size);
@@ -194,50 +201,95 @@ pub const Database = struct {
                 .alloc = dalloc,
                 .mtbls = iters,
                 .queue = queue,
-                .v = null,
             };
         }
 
-        pub fn deinit(mi: *MergeIterator) void {
+        pub fn deinit(ctx: *anyopaque) void {
+            var mi: *MergeIterator = @ptrCast(@alignCast(ctx));
             for (mi.mtbls.items) |item| {
                 item.deinit();
                 mi.alloc.destroy(item);
             }
             mi.mtbls.deinit();
             mi.queue.deinit();
-            mi.* = undefined;
+            mi.alloc.destroy(mi);
         }
 
-        pub fn value(mi: MergeIterator) KV {
-            return mi.v.?;
-        }
-
-        pub fn next(mi: *MergeIterator) !bool {
+        pub fn next(ctx: *anyopaque) ?KV {
+            const mi: *MergeIterator = @ptrCast(@alignCast(ctx));
             for (mi.mtbls.items) |table_iter| {
-                if (try table_iter.next()) {
-                    const kv = table_iter.value();
+                if (table_iter.next()) |kv| {
                     mi.queue.add(kv) catch |err| {
                         debug.print(
                             "merge iter next failure: {s}\n",
                             .{@errorName(err)},
                         );
-                        return false;
+                        return null;
                     };
                 }
             }
 
             if (mi.queue.count() == 0) {
-                return false;
+                return null;
             }
 
             const nxt = mi.queue.remove();
-            mi.*.v = nxt;
-            return true;
+
+            return nxt;
         }
     };
 
-    pub fn iterator(self: *Self) !MergeIterator {
-        return try MergeIterator.init(self.alloc, self);
+    pub fn iterator(self: *Self, alloc: Allocator) !Iterator(KV) {
+        const it = try alloc.create(MergeIterator);
+        it.* = try MergeIterator.init(alloc, self);
+        return Iterator(KV).init(it, MergeIterator.next, MergeIterator.deinit);
+    }
+
+    const ScanIterator = struct {
+        alloc: Allocator,
+        it: Iterator(KV),
+        start_key: []const u8,
+        end_key: []const u8,
+
+        pub fn init(alloc: Allocator, start_key: []const u8, end_key: []const u8, it: Iterator(KV)) ScanIterator {
+            return .{
+                .alloc = alloc,
+                .it = it,
+                .start_key = start_key,
+                .end_key = end_key,
+            };
+        }
+
+        pub fn deinit(ctx: *anyopaque) void {
+            var self: *ScanIterator = @ptrCast(@alignCast(ctx));
+            self.it.deinit();
+            self.alloc.destroy(self);
+        }
+
+        pub fn next(ctx: *anyopaque) ?KV {
+            var self: *ScanIterator = @ptrCast(@alignCast(ctx));
+            while (self.it.next()) |nxt| {
+                switch (mem.order(u8, nxt.key, self.start_key)) {
+                    .lt => continue,
+                    .eq => return nxt,
+                    .gt => {
+                        switch (mem.order(u8, nxt.key, self.end_key)) {
+                            .lt => return nxt,
+                            .eq => return nxt,
+                            .gt => return null,
+                        }
+                    },
+                }
+            }
+            return null;
+        }
+    };
+
+    pub fn scan(self: *Self, alloc: Allocator, start_key: []const u8, end_key: []const u8) !Iterator(KV) {
+        const mit = try self.iterator(alloc);
+        const it = try alloc.create(ScanIterator);
+        it.* = ScanIterator.init(alloc, start_key, end_key, mit);
+        return Iterator(KV).init(it, ScanIterator.next, ScanIterator.deinit);
     }
 
     pub fn write(self: *Self, key: []const u8, value: []const u8) anyerror!void {
@@ -249,9 +301,6 @@ pub const Database = struct {
 
         var mtable = self.mtable.load(.seq_cst);
         if ((mtable.size() + item.len()) >= self.capacity) {
-            // debug.print("freeze and flush current size {d} to add {d} @ capacity {d}...\n", .{
-            //     mtable.size(), item.len(), self.capacity,
-            // });
             try self.freezeAndFlush(mtable);
         }
 
@@ -331,10 +380,12 @@ test "basic functionality with many items" {
     defer db.deinit();
 
     // given
-    const kvs = [3]KV{
+    const kvs = [5]KV{
         KV.init("__key_c__", "__value_c__"),
         KV.init("__key_b__", "__value_b__"),
+        KV.init("__key_d__", "__value_d__"),
         KV.init("__key_a__", "__value_a__"),
+        KV.init("__key_e__", "__value_e__"),
     };
 
     // when
@@ -349,24 +400,25 @@ test "basic functionality with many items" {
     }
 
     // then
-    var iter = try db.iterator();
-    defer iter.deinit();
+    var it = try db.iterator(alloc);
+    defer it.deinit();
 
     var count: usize = 0;
-    while (iter.next() catch false) {
+    while (it.next()) |_| {
         count += 1;
     }
+
     try testing.expectEqual(kvs.len, count);
-}
 
-test "load data" {
-    const alloc = testing.allocator;
+    var items = std.ArrayList(KV).init(alloc);
+    defer items.deinit();
 
-    const db = try databaseFromOpts(alloc, withDataDirOpts("/home/blong14/Developer/git/lib_lsm/.tmp/data"));
-    defer alloc.destroy(db);
-    defer db.deinit();
+    var scan_iter = try db.scan(alloc, "__key_b__", "__key_d__");
+    defer scan_iter.deinit();
 
-    try db.open();
+    while (scan_iter.next()) |nxt| {
+        try items.append(nxt);
+    }
 
-    try testing.expectEqual(33, db.mtables.items.len);
+    try testing.expectEqual(3, items.items.len);
 }
